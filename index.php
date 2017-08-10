cf56cd646189f0768cd5788152f9b1dfbefc04d9
kokuaki <pkokuaki04@gmail.com>
2017-08-10 18:27:58 +0900

    homepageからキュレーション作成

--- index.php
+++ index.php
@@ -8,12 +8,19 @@ set_time_limit(7200);
 
 $controlpage = array();
 
+////////////////////////////////
+////////////////////////////////
+      //  header
+$tosel11 = 2;
+////////////////////////////////
+////////////////////////////////
+
 
 // $rownumber = 2;
 // $rownumber = 1142;
 // $rownumber = 3011;
 // $rownumber = 3251;
-$rownumber = 4001;
+$rownumber = 12;
 
 // $homepage = range(2,116);
 // $homepage = range(117,200);
@@ -21,9 +28,9 @@ $rownumber = 4001;
 // $homepage = range(261,300);
 // $homepage = range(301,350);
 // $homepage = range(326,400);
-$homepage = range(401,550);
+$homepage = range(2,550);
 
-$datainhomepage = range(1,1);
+// $datainhomepage = range(1,1);
 
 // for ($i = 0; $i <= 1000; $i++) {
 // echo $a[$i];
@@ -37,39 +44,13 @@ $datainhomepage = range(1,1);
 date_default_timezone_set('Asia/Tokyo');
 mb_language("Japanese");
 
-// echo '$out = $html->find(\'tr\',0);でH1タグの情報を取り出す<br>';
 
-
-
-
-
-// $stringout0 = $out0->plaintext;
-// $stringout1 = $out0->outertext;
-// $stringout2 = $out0->innertext;
-
-// echo $out0-> outertext;
-// echo $out2-> outertext;
-// echo $out3-> outertext;
-// echo $out4-> outertext;
-// echo $out5-> outertext;
-// echo $out6-> outertext;
-// echo $out7-> outertext;
-// 
-// echo $out8-> innertext;
-// echo $out9-> innertext;
-// echo $out10-> innertext;
-// echo $out11-> innertext;
-
-// print $stringout0;
-
-// $var1 = '';
-// 
-// $var1 = $out0;
-// echo $var1;
 ////////////////////////////////////////////
 ////////////////////////////////////////////
 ////////////////////////////////////////////
-////////////////////////////////////////////
+
+///////////////////設定/////////////////////////
+
 ////////////////////////////////////////////
 ////////////////////////////////////////////
 ////////////////////////////////////////////
@@ -103,10 +84,11 @@ $sheet = $excel->getActiveSheet();
 
 ////////////////////////////////////
 ///////////////////////////////////
-//            header
+//            header12まで
 ////////////////////////////////////
 ///////////////////////////////////
 // セル(A1)に値を入力します。
+
 $sheet->setCellValue('A1', '企業名');
 $sheet->setCellValue('B1', '代表者名');
 $sheet->setCellValue('C1', '資本金');
@@ -114,87 +96,137 @@ $sheet->setCellValue('D1', '従業員数');
 $sheet->setCellValue('E1', '担当者');
 $sheet->setCellValue('F1', '提供可能サービス');
 
+  $headerpage12 = ("http://hp.submit.ne.jp/producer/search/homepage");
 
-$headerpage = ("http://hp.submit.ne.jp/producer/search/homepage");
+  $headerpagefile12 = file_get_html( "$headerpage12" );
+  
+  
+      $urlpartstringout = [
 
-// for ($i = 0; $i <= 5; $i++) {
-//   $companyinfomation = $stringout[$i]; //${$stringout.$i}; 
-//   $sheet->setCellValueByColumnAndRow($i, 2, $companyinfomation);
-//   // echo $i;
-// 
-//   $wordget0 = file_get_contents("http://hp.submit.ne.jp/producer/show/" . "$setwebnumber[$rownumber]");
-//   //文字列の指定の形式に変える
-//   $wordget = mb_convert_encoding($wordget, 'utf-8', 'auto');
-//   //文字列をsimple_html_domのメソッドstr_get_htmlで解析して電子辞書化して取り出せるようにする
-//   $html = str_get_html($wordget);
-// 
-// 
-//   //H1タグの情報を取り出す
-//   $out0 = $html->find('td',0);
-//   $out1 = $html->find('td',4);
-//   $out2 = $html->find('td',5);
-//   $out3 = $html->find('td',6);
-//   $out4 = $html->find('td',7);
-//   $out5 = $html->find('td',8);
-//   // $out6 = $html->find('td',9);
-//   // $out7 = $html->find('td',10);
-//   // $out8 = $html->find('td',11);
-//   // $out9 = $html->find('td',12);
-//   // $out10 = $html->find('td',13);
-//   //////////////////////////////
-//   //////////////////////////////
-// 
-//   $stringout = [
-//     $out0->plaintext,
-//     $out1->plaintext,
-//     $out2->plaintext,
-//     $out3->plaintext,
-//     $out4->plaintext,
-//     $out5->plaintext,
-//     // $out6->plaintext,
-//     // $out7->plaintext,
-//     // $out8->plaintext,
-//     // $out9->plaintext,
-//     // $out10->plaintext
-//   ];
-// 
-//   // $stringout0 = $out0->plaintext;
-//   // $stringout1 = $out1->plaintext;
-//   // $stringout2 = $out2->plaintext;
-//   // $stringout3 = $out3->plaintext;
-//   // $stringout4 = $out4->plaintext;
-//   // $stringout5 = $out5->plaintext;
-//   // $stringout6 = $out6->plaintext;
-//   // $stringout7 = $out7->plaintext;
-//   // $stringout8 = $out8->plaintext;
-//   // $stringout9 = $out9->plaintext;
-//   // $stringout10 = $out10->plaintext;
-// 
-//   // echo $sabmitsiteall[$rownumber];
-//   // echo $out0;
-//   
-// 
-//   for ($i = 0; $i <= 5; $i++) {
-//     $companyinfomation = $stringout[$i]; //${$stringout.$i}; 
-//     $sheet->setCellValueByColumnAndRow($i, 2, $companyinfomation);
-//     // echo $i;
-//   }
-// }
+        $out00chileselect = $headerpagefile12->find('.name a', 0),
+        $out01chileselect = $headerpagefile12->find('.name a', 1),
+        $out02chileselect = $headerpagefile12->find('.name a', 2),
+        $out03chileselect = $headerpagefile12->find('.name a', 3),
+        $out04chileselect = $headerpagefile12->find('.name a', 4),
+        $out05chileselect = $headerpagefile12->find('.name a', 5),
+        $out06chileselect = $headerpagefile12->find('.name a', 6),
+        $out07chileselect = $headerpagefile12->find('.name a', 7),
+        $out08chileselect = $headerpagefile12->find('.name a', 8),
+        $out09chileselect = $headerpagefile12->find('.name a', 9),
+      ];
+
+
+
+      
+        // $out0chileselect = $htmlcontrol->find('.name a', 0);
+        // echo $out0chileselect;
+      $urlpartouthead = [
+        $out00urlWithdrawn = $out00chileselect->href,
+        $out01urlWithdrawn = $out01chileselect->href,
+        $out02urlWithdrawn = $out02chileselect->href,
+        $out03urlWithdrawn = $out03chileselect->href,
+        $out04urlWithdrawn = $out04chileselect->href,
+        $out05urlWithdrawn = $out05chileselect->href,
+        $out06urlWithdrawn = $out06chileselect->href,
+        $out07urlWithdrawn = $out07chileselect->href,
+        $out08urlWithdrawn = $out08chileselect->href,
+        $out09urlWithdrawn = $out09chileselect->href,
+      ];
+  
+
+      for ($urlsetciclehead = 0; $urlsetciclehead <= 9; $urlsetciclehead++) {
+
+        $wordgethead = file_get_contents("http://hp.submit.ne.jp" . "$urlpartouthead[$urlsetciclehead]");
+        //文字列の指定の形式に変える
+        $wordgethead = mb_convert_encoding($wordgethead, 'utf-8', 'auto');
+        //文字列をsimple_html_domのメソッドstr_get_htmlで解析して電子辞書化して取り出せるようにする
+        $htmlhead = str_get_html($wordgethead);
+        
+        
+        //H1タグの情報を取り出す
+        $out00 = $htmlhead->find('td',0);
+        // echo "out0は通った";
+        $out01 = $htmlhead->find('td',4);
+        $out02 = $htmlhead->find('td',5);
+        $out03 = $htmlhead->find('td',6);
+        $out04 = $htmlhead->find('td',7);
+        $out05 = $htmlhead->find('td',8);
+
+        
+        $stringouthead = [
+          $out00->plaintext,
+          $out01->plaintext,
+          $out02->plaintext,
+          $out03->plaintext,
+          $out04->plaintext,
+          $out05->plaintext,
+        
+        ];
+        
+        
+
+            for ($headi = 0; $headi <= 5; $headi++) {
+              $companyinfomation = $stringouthead[$headi]; //${$stringout.$i}; 
+              $sheet->setCellValueByColumnAndRow($headi, $tosel11, $companyinfomation);
+              // echo $i;
+            }
+            $tosel11++;
+            $htmlhead->clear(); 
+            unset($htmlhead);
+      
+
+  
+
+      }
+      $writer = PHPExcel_IOFactory::createWriter($excel, "Excel2007");
+      $writer->save("PHPExcel.xlsx");
+      $headerpagefile12->clear(); 
+      unset($headerpagefile12);
+
+
+
 ////////////////////////////////////
 ///////////////////////////////////
 //            header
 ////////////////////////////////////
 ///////////////////////////////////
-// セル名ではなく数値(座標)で値を入力する方法もあります
-// 第1引数は列を「0から」数えた数値、第2引数は行を「1から」数えた数値になります
-// 下記はA2に値を入力しています
-// $sheet->setCellValueByColumnAndRow(0, 2, "$stringout0");
-// 
-// for ($i = 1; $i <= 1; $i++) {
-//     echo $i;
-// }
-// $rownumber = 2;
-// $rownumber = 1592;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 foreach($homepage as $homepageset){
   ////////////////////////////////////
@@ -209,53 +241,8 @@ foreach($homepage as $homepageset){
 
   echo $homepageset;
   $htmlcontrol = file_get_html( "$originalcontrolpage" );
-  // $out0select = $htmlcontrol->find('.mgBtm30 li', 0);
-  // echo $out0select;
-  // foreach($datainhomepage as $datainhomepagenumbar){
-    
-      
-      
-      
-      // $stringout = [
-      //   $out0urlWithdrawn->plaintext,
-        // $out6->plaintext,
-        // $out7->plaintext,
-        // $out8->plaintext,
-        // $out9->plaintext,
-        // $out10->plaintext
-      // ];
-      
-      // $stringout0 = $out0->plaintext;
-      // $stringout1 = $out1->plaintext;
-      // $stringout2 = $out2->plaintext;
-      // $stringout3 = $out3->plaintext;
-      // $stringout4 = $out4->plaintext;
-      // $stringout5 = $out5->plaintext;
-      // $stringout6 = $out6->plaintext;
-      // $stringout7 = $out7->plaintext;
-      // $stringout8 = $out8->plaintext;
-      // $stringout9 = $out9->plaintext;
-      // $stringout10 = $out10->plaintext;
-
-      // echo $sabmitsiteall[$rownumber];
-      // echo $out0;
-      
-        // $companyinfomation = $stringout0; 
-        //${$stringout.$i}; 
-        // echo $i;
-      
-
-      // $writer = PHPExcel_IOFactory::createWriter($excel, "Excel2007");
-      ////////////////////////////////////
-      ////////////////////////////////////
-      ////////////////////////////////////
-      ////////////////////////////////////
-      ////////////////////////////////////
-      ////////////////////////////////////
-
-      
-      // foreach($number as $rownumber){
-      // for ($hometag = 0; $hometag <= 9; $hometag++) {
+  
+  
       $urlpartstringout = [
 
         $out0chileselect = $htmlcontrol->find('.name a', 0),
@@ -287,22 +274,7 @@ foreach($homepage as $homepageset){
         $out8urlWithdrawn = $out8chileselect->href,
         $out9urlWithdrawn = $out9chileselect->href,
       ];
-        // echo "$urlpartout[0]";
-        // echo "$urlpartout[1]";
-        // echo "$urlpartout[2]";
-        // echo "$urlpartout[3]";
-        // echo "$urlpartout[4]";
-        // echo "$urlpartout[5]";
-        // echo "$urlpartout[6]";
-        // echo "$urlpartout[7]";
-        // echo "$urlpartout[8]";
-        // echo "$urlpartout[9]";
-
-        // $originalcontrolpage = "http://hp.submit.ne.jp/producer/search/homepage?page=" . "$rownumber + 1";
-
-        //url先のコードを文字列としてget
-        // $wordget = file_get_contents("http://hp.submit.ne.jp/producer/search/homepage" . ($rownumber ? ("?page=". ($rownumber+1)) : ""));
-        //url先のコードを文字列としてget
+      
         for ($urlsetcicle = 0; $urlsetcicle <= 9; $urlsetcicle++) {
 
           $wordget = file_get_contents("http://hp.submit.ne.jp" . "$urlpartout[$urlsetcicle]");
@@ -320,13 +292,7 @@ foreach($homepage as $homepageset){
           $out3 = $html->find('td',6);
           $out4 = $html->find('td',7);
           $out5 = $html->find('td',8);
-          // $out6 = $html->find('td',9);
-          // $out7 = $html->find('td',10);
-          // $out8 = $html->find('td',11);
-          // $out9 = $html->find('td',12);
-          // $out10 = $html->find('td',13);
-          //////////////////////////////
-          //////////////////////////////
+    
           
           $stringout = [
             $out0->plaintext,
@@ -335,27 +301,10 @@ foreach($homepage as $homepageset){
             $out3->plaintext,
             $out4->plaintext,
             $out5->plaintext,
-            // $out6->plaintext,
-            // $out7->plaintext,
-            // $out8->plaintext,
-            // $out9->plaintext,
-            // $out10->plaintext
+          
           ];
           
-          // $stringout0 = $out0->plaintext;
-          // $stringout1 = $out1->plaintext;
-          // $stringout2 = $out2->plaintext;
-          // $stringout3 = $out3->plaintext;
-          // $stringout4 = $out4->plaintext;
-          // $stringout5 = $out5->plaintext;
-          // $stringout6 = $out6->plaintext;
-          // $stringout7 = $out7->plaintext;
-          // $stringout8 = $out8->plaintext;
-          // $stringout9 = $out9->plaintext;
-          // $stringout10 = $out10->plaintext;
-
-          // echo $sabmitsiteall[$rownumber];
-          // echo $out0;
+          
           for ($i = 0; $i <= 5; $i++) {
             $companyinfomation = $stringout[$i]; //${$stringout.$i}; 
             $sheet->setCellValueByColumnAndRow($i, $rownumber, $companyinfomation);
@@ -366,7 +315,7 @@ foreach($homepage as $homepageset){
           $html->clear(); 
           unset($html);
 
-      }
+        }
       // }
       $writer = PHPExcel_IOFactory::createWriter($excel, "Excel2007");
       $writer->save("PHPExcel.xlsx");
